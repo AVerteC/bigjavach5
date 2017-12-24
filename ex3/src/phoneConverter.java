10d8f31b53df8655ac78a8d59f23e8df8089594c
Alan Chen <alan.r.chen@gmail.com>
2017-12-23 22:50:43 -0800

    finished letter to digit

--- ex3/src/phoneConverter.java
+++ ex3/src/phoneConverter.java
@@ -1,6 +1,7 @@
 public class phoneConverter {
     public static void main(String args[]) {
         phoneNumber numconverter = new phoneNumber();
-        numconverter.numberConverter("COMCAST");
+        String x = numconverter.numberConverter("1800COMCAST ");
+        System.out.println(x);
     }
 }
--- ex3/src/phoneNumber.java
+++ ex3/src/phoneNumber.java
@@ -1,25 +1,46 @@
 public class phoneNumber {
 
     public phoneNumber(){
-        String output = "";
     }
 
-    public void numberConverter(String letters) {
+    public String numberConverter(String letters) {
+        String input = "";
+        String output = "";
         for (int c = 0; c < letters.length(); c++) {
             char letter = letters.charAt(c);
+            input = Character.toString(letter);
             //2 = ABC 3 = DEF 4 = GHI 5 = JKL
             //6 = MNO 7 = PRS 8 = TUV 9 = WXY
 
-                if ((letter.contains("A")||(letter.contains("B")||(letter.contains("C")){
-                    System.out.print(2);
+                if (input.contains("A")||input.contains("B")||input.contains("C")) {
+                    output = (output + "2");
+                    //System.out.print(output);
+                }
+                else if (input.contains("D")||input.contains("E")||input.contains("F")) {
+                    output = (output + "3");
+                    //output = Integer.parseInt(output);
+                }
+                else if (input.contains("G")||input.contains("H")||input.contains("I")) {
+                    output = (output + "4");
+                }
+                else if (input.contains("J")||input.contains("K")||input.contains("L")) {
+                    output = (output + "5");
+                }
+                else if (input.contains("M")||input.contains("N")||input.contains("O")) {
+                    output = (output + "6");
+                }
+                else if (input.contains("P")||input.contains("Q")||input.contains("R")||input.contains("S")) {
+                    output = (output + "7");
+                }
+                else if (input.contains("V")||input.contains("T")||input.contains("U")) {
+                    output = (output + "8");
+                }
+                else if (input.contains("W")||input.contains("X")||input.contains("Y")||input.contains("Z")) {
+                    output = (output + "9");
                 }
 
-                else if((letter.contains("D")||(letter.contains("E")||(letter.contains("F")){
-                    System.out.print(3);
-                }
 
-            System.out.println(letter);
         }
-
+        return output;
     }
 }
