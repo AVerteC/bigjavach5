b1680d39c9ecb0e00b9ac252423b37d1c2efabd5
Alan Chen <alan.r.chen@gmail.com>
2018-01-21 21:51:38 -0800

    finished worthy combolock

--- ifthenp_5.1/src/CombinationLock.java
+++ ifthenp_5.1/src/CombinationLock.java
@@ -4,11 +4,15 @@ public class CombinationLock {
     String locknums = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private String Combo = "";
     private String longCombo = "";
+    private String extrasYo = "";
 
     public char nabLetter(){
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         char c = input.charAt(0);
+        if (Character.isLetter(c)!= true){
+            System.out.println("Please input a letter.");
+        }
         return c;
     }
 
@@ -28,21 +32,28 @@ public class CombinationLock {
     }
 
     public void setLetter(char letter) {
-        if (Character.isLetter(letter)!= true){
-            System.out.println("Please input a letter.");
-        }
-        if (!Character.isUpperCase(letter)) {
-            letter = Character.toUpperCase(letter);
-        }
-        if (Combo.length() < 3) {
-            this.Combo = this.Combo + Character.toString(letter);
+        if (Character.isLetter(letter) != true) {
+            //System.out.println("Please input a letter.");
+            this.extrasYo = this.extrasYo + Character.toString(letter);
         }
         else {
-            this.Combo = this.Combo + Character.toString(letter);
-            this.longCombo = Character.toString(this.Combo.charAt(this.Combo.length() - 3)) +
-                    Character.toString(this.Combo.charAt(this.Combo.length() - 2)) +
-                    Character.toString(this.Combo.charAt(this.Combo.length() - 1));
+            if (!Character.isUpperCase(letter)) {
+                letter = Character.toUpperCase(letter);
+            }
+            if (Combo.length() < 3) {
+                this.Combo = this.Combo + Character.toString(letter);
+            }
+            else {
+                this.Combo = this.Combo + Character.toString(letter);
+                this.longCombo = Character.toString(this.Combo.charAt(this.Combo.length() - 3)) +
+                        Character.toString(this.Combo.charAt(this.Combo.length() - 2)) +
+                        Character.toString(this.Combo.charAt(this.Combo.length() - 1));
+            }
         }
+    }
+
+    public int comboLength(){
+        return Combo.length();
     }
 
     /*public void getCombo() {
@@ -56,44 +67,43 @@ public class CombinationLock {
     public static void main(String [] args ){
         CombinationLock CLock = new CombinationLock();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please one letter to set the combination lock.");
-        CLock.setLetter(CLock.nabLetter());
-
-        System.out.println("Please another letter to set the combination lock.");
-        CLock.setLetter(CLock.nabLetter());
-
-        System.out.println("Please one more letter to set the combination lock.");
-        CLock.setLetter(CLock.nabLetter());
+        while (CLock.comboLength() < 3) {
+            System.out.println("Please enter a letter to set the combination lock.");
+            CLock.setLetter(CLock.nabLetter());
+        }
         //CLock.getCombo();
-        System.out.println("Do you want to add more [L]etters, [U]nlock the combination lock, or [E]xit?");
-        System.out.println("Please enter L/U/E");
-        String answer = sc.nextLine();
-        while (answer.equals("E") == false) {
-
+        String answer = "A";
+        while (!answer.equals("E")) {
+            System.out.println("Do you want to add more [L]etters, [U]nlock the combination lock, or [E]xit?");
+            System.out.println("Please enter L/U/E .");
+            answer = sc.nextLine();
 
             if (answer.equals("L")) {
                 System.out.println("Enter another letter for the combination");
                 CLock.setLetter(CLock.nabLetter());
-            } else if (answer.equals("U")) {
-                System.out.println("Enter the combination.");
-                //CLock.getCombo();
-                String input = sc.nextLine();
-                if (CLock.unLock(input) == false) {
-                    System.out.println("The combination is incorrect. Try again.");
-                    CLock.unLock(sc.nextLine());
-                }
-
-                    if (CLock.unLock(input) == true) {
-                        System.out.println("The lock has been unlocked.");
-                        System.out.println("The lock has also been closed.");
-                    }
-
             }
-            System.out.println("Do you want to add more [L]etters, [U]nlock the combination lock, or [E]xit?");
-            System.out.println("Please enter L/U/E");
-            answer = sc.nextLine();
+            if (answer.equals("U")) {
+                //CLock.getCombo();
+                System.out.println("Enter the combination.");
+                String input = sc.nextLine();
+
+                if (CLock.unLock(input)) {
+                    System.out.println("The lock has been unlocked.");
+                    System.out.println("The lock has also been closed.");
+
+                }
+                if (!CLock.unLock(input)) {
+                    System.out.println("The combination is incorrect.");
+                }
+            }
+
         }
 
 
     }
+
+
 }
+
+
+
